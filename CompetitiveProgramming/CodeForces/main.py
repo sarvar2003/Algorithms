758ebe77daecce5475619b76c29f672fddd5a1c4
Sarvarbek <saravarjuraev@gmail.com>
2023-02-21 12:57:40 +0100

    CodeForeces

--- CompetitiveProgramming/CodeForces/main.py
+++ CompetitiveProgramming/CodeForces/main.py
@@ -1,15 +1,30 @@
-def solve():
-   steps = int(input()) 
-   five = steps // 5
-   steps = steps % 5
-   four = steps // 4
-   steps = steps % 4
-   three = steps // 3
-   steps = steps % 3
-   two = steps // 2
-   steps = steps % 2
-   one = steps//1
+def solve(nine, ten, n):
+   atNine, atTen, cnt = {}, {}, 0
 
-   print(five + four + three + two + one)
+   for i in range(n):
+      atNine[nine[i]] = i
+   
+   for i in range(n):
+      atTen[ten[i]] = i
 
-solve()
+   # print(atNine, atTen)
+
+   for i in range(n-1):
+      if atNine[nine[i]] < atTen[nine[i+1]] and atNine[nine[i]] != atTen[nine[i]]:
+         cnt += 1
+
+   return cnt
+
+
+for _ in range(int(input())):
+   n = int(input())
+   a = list(map(int, input().split()))
+   b = list(map(int, input().split()))
+
+   print(solve(a, b, n) )
+
+"""
+5
+1 4 2 5 3
+1 3 4 2 5
+"""
